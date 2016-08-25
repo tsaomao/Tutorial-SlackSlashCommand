5c7a67dfe7e41a4ef875f6892b890ff2ea53fbdc
Malcolm Gin <malcomgin@SFO-M-MGIN01.local>
2016-08-25 13:11:22 -0700

    Just comments.

---
+++ tut1.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+	REQUIREMENTS
+
+	* Take a Slack Slash Command
+	* Check querystring token or header token for expected validity
+	* Parse Command
+	* Forward Command to appropriate third party web service
+	* Return results via Slack Incoming Webhook
+	*/
+
+?>
